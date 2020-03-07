52fc96e4d5b15bfd4e56fc9c9b02d98b97fb3d4a
lily-shen <921180142@qq.com>
2020-03-07 20:41:31 +0800

    Signed-off-by: lily-shen <921180142@qq.com>

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -9,17 +9,13 @@
 			</view>
 		</view>
 		<!--star  banner -->
-		
-		<video autoplay="true"   id="myVideo" src="https://img.cdn.aliyun.dcloud.net.cn/guide/uniapp/%E7%AC%AC1%E8%AE%B2%EF%BC%88uni-app%E4%BA%A7%E5%93%81%E4%BB%8B%E7%BB%8D%EF%BC%89-%20DCloud%E5%AE%98%E6%96%B9%E8%A7%86%E9%A2%91%E6%95%99%E7%A8%8B@20181126.mp4"
-		 @error="videoErrorCallback" controls poster="../../static/images/index-banner.jpg"></video>
 		<view class="banner">
 			<swiper class="swiper" :indicator-color='color' :indicator-dots="indicatorDots" :autoplay="autoplay" :interval="interval"
 			 :duration="duration" :indicator-active-color='color' :loop='loop'>
 				<swiper-item>
 					<view class="swiper-item  positionr ">
 						<image src="../../static/images/index-banner.jpg" mode="widthFix" class="w100 "></image>
-						<!-- <video id="myVideo" src="https://img.cdn.aliyun.dcloud.net.cn/guide/uniapp/%E7%AC%AC1%E8%AE%B2%EF%BC%88uni-app%E4%BA%A7%E5%93%81%E4%BB%8B%E7%BB%8D%EF%BC%89-%20DCloud%E5%AE%98%E6%96%B9%E8%A7%86%E9%A2%91%E6%95%99%E7%A8%8B@20181126.mp4"
-						 @error="videoErrorCallback" controls poster="../../static/images/index-banner.jpg"></video> -->
+						
 					</view>
 				</swiper-item>
 				<swiper-item>
@@ -35,6 +31,11 @@
 			</swiper>
 		</view>
 		<!--end  banner -->
+		
+		<view class="margin-b20" >
+			<video  class="w100"  autoplay="true"   id="myVideo" src="https://img.cdn.aliyun.dcloud.net.cn/guide/uniapp/%E7%AC%AC1%E8%AE%B2%EF%BC%88uni-app%E4%BA%A7%E5%93%81%E4%BB%8B%E7%BB%8D%EF%BC%89-%20DCloud%E5%AE%98%E6%96%B9%E8%A7%86%E9%A2%91%E6%95%99%E7%A8%8B@20181126.mp4"
+			 @error="videoErrorCallback"  poster="../../static/images/index-banner.jpg"></video>
+		</view>
 
 		<!-- index-item1 -->
 		<view class="index-item1">
@@ -273,7 +274,7 @@
 	}
 
 	.banner {
-		padding: 60rpx 0;
+		padding:40rpx 0;
 	}
 
 	.video {
